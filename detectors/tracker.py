d4b9e650a07d67d3e9a94616e2398fff8ab94498
Dhruv-mat <dhruv.mathur0204@gmail.com>
2026-07-26 12:01:49 +0530

    Update tracker.py

--- detectors/tracker.py
+++ detectors/tracker.py
@@ -16,7 +16,14 @@ def is_locked(person,cam_x_cent , cam_y_cent):
         print("centered")
         return True
     else:
-        print("not centered")
+        print(f"Camera center : ({cam_x_cent}, {cam_y_cent})")
+
+        print(f"Person left   : {left}")
+        print(f"Person right  : {right}")
+        print(f"Person top    : {top}")
+        print(f"Person bottom : {bottom}")
+
+        print(f"Person center : ({person.center_x}, {person.center_y})")
         return False
 
 def select(persons):
