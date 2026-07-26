6520f3768d635700e342e3cba84200354be728b1
Dhruv-mat <dhruv.mathur0204@gmail.com>
2026-07-26 11:08:25 +0530

    Update main.py

--- detectors/main.py
+++ detectors/main.py
@@ -1,3 +1,21 @@
-from lidar import hi
+import lidar
+import camera
+import tracker
 
-hi()
+cam_x_cent , cam_y_cent = camera.initialize()
+
+
+lidar.initialize()
+
+
+
+while True:
+    persons = camera.get_people()
+
+    target = tracker.select(persons)
+
+    if target is None:
+        continue
+
+    if tracker.is_locked(target,cam_x_cent , cam_y_cent):
+        distance = lidar.get_distance()
